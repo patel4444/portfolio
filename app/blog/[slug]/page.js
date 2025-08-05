8ceeaeb50745210526a31b41cf132449cb9ca8b2
patel4444 <your-email@example.com>
2025-08-05 14:13:18 +0530

    Fix blog slug page

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,25 +1,27 @@
-// @flow strict
 import { personalData } from "@/utils/data/personal-data";
 
 async function getBlog(slug) {
-  const res = await fetch(`https://dev.to/api/articles/${personalData.devUsername}/${slug}`)
+  const res = await fetch(`https://dev.to/api/articles/${personalData.devUsername}/${slug}`);
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data')
+    throw new Error('Failed to fetch data');
   }
 
   const data = await res.json();
   return data;
-};
+}
 
-async function BlogDetails({params}) {
+export default async function BlogDetails({ params }) {
   const slug = params.slug;
   const blog = await getBlog(slug);
- 
+
   return (
-    <div>
+    <div style={{ padding: "2rem" }}>
+      <h1>{blog.title}</h1>
+      <p>
+        <strong>Published at:</strong> {new Date(blog.published_at).toLocaleDateString()}
+      </p>
+      <div dangerouslySetInnerHTML={{ __html: blog.body_html }} />
     </div>
   );
-};
-
-export default BlogDetails;
+}
